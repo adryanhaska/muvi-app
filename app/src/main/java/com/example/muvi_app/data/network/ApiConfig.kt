fb76093a8bad1e30e99a7fd6eb7a462e2345de39
Muhammad Adryan Haska Putra <muhammad.adryan@ui.ac.id>
2024-06-14 12:58:18 +0700

    pull

--- app/src/main/java/com/example/muvi_app/data/network/ApiConfig.kt
+++ app/src/main/java/com/example/muvi_app/data/network/ApiConfig.kt
@@ -1,17 +1,32 @@
 package com.example.muvi_app.data.network
 
+import com.example.muvi_app.data.pref.UserPreference
 import com.google.gson.GsonBuilder
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    fun getApiService(): ApiService {
-        val loggingInterceptor =
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    fun getApiService(userPreference: UserPreference): ApiService {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val tokenInterceptor = Interceptor { chain ->
+            val originalRequest = chain.request()
+            val token = runBlocking { userPreference.getToken() }
+            val requestBuilder = originalRequest.newBuilder()
+                .header("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(requestBuilder)
+        }
+
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor(tokenInterceptor)
             .build()
         val retrofit = Retrofit.Builder()
             .baseUrl("https://c24-mr01-server-arxlnabp3q-et.a.run.app/api/v1/")
